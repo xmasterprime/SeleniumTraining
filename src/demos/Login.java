012ffe6f32e9b501db7c26758ec57a5d228fcdc2
dustinsison <dustin.sison@moovweb.com>
2019-07-29 13:28:32 -0700

    First commit

---
+++ src/demos/Login.java
@@ -0,0 +1,26 @@
+package demos;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Login {
+	
+	public static void main(String[] args) {
+		// 1. Define the web driver
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Dustin Sison\\eclipse-workspace\\Software\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		// 2. Open web browser, and navigate to http://sdettraining.com/trguitransactions/AccountManagement.aspx
+		driver.get("http://sdettraining.com/trguitransactions/AccountManagement.aspx");
+		
+		// 3. Enter email address
+		
+		// 4. Enter password
+		
+		// 5. Click login
+		
+		// 6. Get Confirmation
+		
+		// 7. Close Browser
+	}
+}
